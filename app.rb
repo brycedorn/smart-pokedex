462dcedf5a82f84fe89be4c0f2de9ac5d05ba0ca
Bryce Dorn <brycedorn@gmail.com>
2014-05-05 13:05:59 -0500

    adds routes to pages

--- app.rb
+++ app.rb
@@ -6,6 +6,14 @@ class App < Sinatra::Base
   get '/' do
     haml :index
   end
+
+  get '/about' do
+    haml :about
+  end
+
+  get '/contact' do
+    haml :contact
+  end
   
   # Start the server if ruby file executed directly
   run! if app_file == $0
